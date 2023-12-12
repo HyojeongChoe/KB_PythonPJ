f10ac7bcd53c56117d4e55d789542d5c78954412
mulbom <87961148+mulbom@users.noreply.github.com>
2023-12-12 10:34:54 +0900

    Update gptConnect.py

--- KBJ/gptConnect.py
+++ KBJ/gptConnect.py
@@ -24,10 +24,7 @@ def get_question():
             "apikey": "<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>",
             "messages" :[{"role": "user", "content": request_data['action']['params']['question']}],
             "userdata": [["user", request_data['userRequest']['user']['id']]]},
-            headers={"apikey":"<Asyncia API KEY>"}, timeout=0.3) #asyncia : 웹훅 사용하려면 필요
-            # 어센시아 홈페이지에서 api신청 - 구름컨테이너 주소 필요함 - api키 재확인 불가하니 메모든 카카오톡이든 저장해두기 / 터미널에서 파일명 입력 필요 *사용중에 터미널 열어둬야함 / 용량 제한 있으니 용량 부족 시 증가신청 필요
-            # 스킬 추가 해야줘야함 / 시나리오 질문블럭, 답변블럭 ai는 파라미터도 추가해줘야함 / 되묻기 최대횟수 지정 필요 / question<<질문블럭 / ans<<답변블럭
-            # 답변블럭 호출을 안하면 웹훅한걸 안보여줌...
+            headers={"apikey":"<Asyncia API KEY>"}, timeout=0.3)
     except requests.exceptions.ReadTimeout:
         pass
     return jsonify(response)
